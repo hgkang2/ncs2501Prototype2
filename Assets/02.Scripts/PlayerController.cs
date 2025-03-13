69008a6e280a662244fbc396016a2802f090b710
hgkang2 <hgkang2hank@naver.com>
2025-03-13 15:49:38 +0900

    동물충돌

--- Assets/02.Scripts/PlayerController.cs
+++ Assets/02.Scripts/PlayerController.cs
@@ -1,36 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject projectilePrefab;
     public float horizontalInput;
-    public float speed = 25.0f; 
+    public float speed = 25.0f;
     public float xRange = 15.0f;
+
     void Start()
     {
         
     }
 
-
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(projectilePrefab,transform.position,projectilePrefab.transform.rotation);
-        }
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right*horizontalInput*Time.deltaTime*speed);
+        
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
         // x좌표를 넘지 않게 한다
-        if(transform.position.x < -xRange)
+        if (transform.position.x < -xRange) 
         {
-            transform.position = new Vector3(-xRange,transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
         if (transform.position.x > xRange)
         {
-             transform.position = new Vector3(xRange,transform.position.y, transform.position.z);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        // 발사체
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, 
+                projectilePrefab.transform.rotation);
         }
     }
 }
